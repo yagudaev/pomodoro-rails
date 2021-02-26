e07163cbcd2eef600662c270e3ce5fa658ed8dad
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-02-25 21:39:12 -0800

    Good bye turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,10 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import MatestackUiCore from 'matestack-ui-core'
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 require("css/application.scss")
-import MatestackUiCore from 'matestack-ui-core'
