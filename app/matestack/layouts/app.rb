9faa8bf5a2fab5fbe0093a45f43de26b7ccfa781
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-02-25 22:14:17 -0800

    get initial skeleton going

--- app/matestack/layouts/app.rb
+++ app/matestack/layouts/app.rb
@@ -1,7 +1,7 @@
 class Layouts::App < Matestack::Ui::App
   def response
     heading size: 1, text: "Pomodoro", class: "mt-2 ml-2"
-    main do
+    main class: "px-4 py-2" do
       yield_page
     end
   end
--- app/matestack/pages/timer.rb
+++ app/matestack/pages/timer.rb
@@ -1,7 +1,8 @@
 class Pages::Timer < Matestack::Ui::Page
   def response
     div do
-      "hello world"
+      plain "00:00"
+      button text: "Play", class: "block mt-2 bg-green-300 px-3 py-1 rounded-sm"
     end
   end
 end
